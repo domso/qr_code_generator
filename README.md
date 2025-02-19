68332b7616ae9c8b4912d2a11c7ad3d9b85d6930
domso <domso94@googlemail.com>
2025-02-20 00:11:47 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# QR Code Generator
+
+## Overview
+
+This C++ tool generates a QR code based on input data and prints it to the terminal. 
+- Generates QR codes from input strings.
+- Prints the QR code directly to the terminal.
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/domso/qr-code-generator.git
+   cd qr-code-generator
+   ```
+
+2. Build the project using CMake:
+   ```bash
+   mkdir build
+   cd build
+   cmake ..
+   make
+   ```
+
+## Usage
+
+To generate a QR code, run the executable with the desired input string as an argument:
+
+```bash
+./qr_code_generator "Your input string here"
+```
+
+### Example Execution
+
+<p float="left">
+  <img src="/examples/helloworld.png" width="100%"/>
+</p>
--- generators/print_generator.cpp
+++ generators/print_generator.cpp
@@ -3,26 +3,34 @@
 #include <iostream>
 
 void print_generator::apply(const qr_code& code) const {
+    //auto white = "⬜";
+    //auto black = "⬛";
+    //auto reset = "";
+
+    auto white = "\033[47m  ";
+    auto black = "\033[40m  ";
+    auto reset = "\033[0m";
+
     for (int x = 0; x < code.area().width() + 2; x++) {
-        std::cout << "\033[47m  ";
+        std::cout << white;
     }
-        std::cout << "\033[0m" << std::endl;
+    std::cout << reset << std::endl;
     for (int y = 0; y < code.area().height(); y++) {
-        std::cout << "\033[47m  ";
+        std::cout << white;
         for (int x = 0; x < code.area().width(); x++) {
             if (code.area().is_set(x, y)) {
-                std::cout << "\033[40m  ";
+                std::cout << black;
             } else if (code.area().is_blocked(x, y)) {
-                std::cout << "\033[47m  ";
+                std::cout << white;
             } else {
-                std::cout << "\033[47m  ";
+                std::cout << white;
             }
         }
-        std::cout << "\033[47m  ";
-        std::cout << "\033[0m" << std::endl;
+        std::cout << white;
+        std::cout << reset << std::endl;
     }
     for (int x = 0; x < code.area().width() + 2; x++) {
-        std::cout << "\033[47m  ";
+        std::cout << white;
     }
-        std::cout << "\033[0m" << std::endl;
+    std::cout << reset << std::endl;
 }
